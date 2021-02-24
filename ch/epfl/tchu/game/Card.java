1ce720e2c18aa8690cba7251c60554750cd4c4a9
Alberto <albicento.ac@gmail.com>
2021-02-24 14:00:21 +0100

    Clean project initialized

--- ch/epfl/tchu/game/Card.java
+++
@@ -1,6 +0,0 @@
-package ch.epfl.tchu.game;
-
-public enum Card { 
-    BLACK, VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED, WHITE, LOCOMOTIVE;
-    
-}
---
+++ src/ch/epfl/tchu/Prova.java
@@ -0,0 +1,7 @@
+package ch.epfl.tchu;
+
+public class Prova {
+    public static void main(String[] args){
+        System.out.println("slkdjfalk");
+    }
+}
