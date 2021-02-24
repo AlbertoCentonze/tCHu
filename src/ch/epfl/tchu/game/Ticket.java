c448159032436262df8d627ec0695e7351275006
EmmaPoggiolini <79009806+EmmaPoggiolini@users.noreply.github.com>
2021-02-24 22:34:27 +0100

    added author, edited constructors, created all the new methods and attributes

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -1,14 +1,20 @@
 package ch.epfl.tchu.game;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import ch.epfl.tchu.Preconditions;
+
+import java.util.*;
+
+/**
+ * @author Emma Poggiolini (330757)
+ */
 
 public final class Ticket implements Comparable<Ticket> {
 
     // list of trips written on the ticket
-    private List<Trip> trips = new List<Trip>();
+    private final List<Trip> trips = new List<Trip>();
+
+    // text on the ticket
+    private final String text = null;
 
     // doesn't specify to put public (?)
     /**
@@ -17,20 +23,73 @@ public final class Ticket implements Comparable<Ticket> {
      * @param trips
      */
     public Ticket(List<Trip> trips) {
+        // checking if list is empty
+        Preconditions.checkArgument(trips.isEmpty());
+        // TODO check that each departure station has the same name (need class Trip)
+
         for(Trip trip : trips) {
             this.trips.add(trip);
         }
+
+        // stock visual representation of ticket
+        text = computeText();
     }
 
     /**
      * secondary constructor
+     * creates a single-trip ticket
      */
     public Ticket(Station from, Station to, int points) {
-        // need class Trip to fill in
+        // TODO need class Trip to fill in
+        this(List.of(new Trip(from, to, points)));
     }
 
-    @Override 
+    /**
+     * paste the visual representation of the ticket
+     * @return (String) text
+     */
+    public String text() {return text;}
+
+    /**
+     * create String of visual representation of the ticket
+     * called in the primary constructor
+     * @return (String) visual representation of ticket
+     */
+    private static String computeText() {
+        // name of the departure-station
+        String fromStation = trips.get(0).getFrom().toString(); // TODO once class Trip get Station from
+
+        // create TreeSet and add all destination-stations
+        TreeSet<String> names = new TreeSet<>();
+        for(Trip trip : trips) {
+            names.add(trip.getTo().toString() + " (" + trip.getPoints().toString() + ")"); // TODO once class Trip get Station to
+        }
+
+        // return String with visual representation of ticket
+        // chain name of departure-station to list of destination-stations
+        if(trips.size() = 1) {
+            return String.format("%s - %s",fromStation,names);
+        } else {
+            return String.format("%s - {%s}",fromStation,String.join(", ",names));
+        }
+    }
+
+    /**
+     * points attributed once a trip on the ticket is finished
+     * @param (StationConnectivity) connectivity
+     * @return (int) points
+     */
+    public int points(StationConnectivity connectivity) {
+        if(connectivity.connected()) {
+            // TODO finish method
+        }
+    }
+
+    @Override
     public int compareTo(Ticket that) {
-
+        return this.text().compareTo(that.text());
     }
+
+    @Override
+    public String toString() {return text();}
 }
