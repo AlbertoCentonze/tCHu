d9f5cce32a49728d470f010f38e92a7138bea08d
EmmaPoggiolini <79009806+EmmaPoggiolini@users.noreply.github.com>
2021-02-24 15:34:34 +0100

    Added new methods to Card and created class Color because used in Card

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -12,5 +12,40 @@ public enum Card {
     public static final int COUNT = ALL.size();
 
     // list of all railcar cards
-    public final static List<Card> CARS = List.of(Card.values()); //remove
+    public final static List<Card> CARS = List.of(ALL.remove(COUNT)); //remove
+
+    // color of card
+    // null if locomotive
+    private Color color;
+
+    public static Card of(Color color) {
+        return CARS.get(color.ordinal());
+    }
+
+    public Color color() {
+        return color; // is defensive copy necessary ??
+    }
+
+    private Card() {    // switch case too repetitive
+        switch (this) {
+            case BLACK:
+                this.color = Color.BLACK;
+            case VIOLET:
+                this.color = Color.VIOLET;
+            case BLUE:
+                this.color = Color.BLUE;
+            case GREEN:
+                this.color = Color.GREEN;
+            case YELLOW:
+                this.color = Color.YELLOW;
+            case ORANGE:
+                this.color = Color.ORANGE;
+            case RED:
+                this.color = Color.RED;
+            case WHITE:
+                this.color = Color.WHITE;
+            default:
+                this.color = null;
+        }
+    }
 }
---
+++ src/ch/epfl/tchu/game/Color.java
@@ -0,0 +1,13 @@
+package ch.epfl.tchu.game;
+
+import java.util.List;
+
+public enum Color {
+    BLACK, VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED, WHITE;
+
+    // list of all the values of Card
+    public static final List<Color> ALL = List.of(Color.values());
+
+    // number of values in the enumeration
+    public static final int COUNT = ALL.size();
+}
