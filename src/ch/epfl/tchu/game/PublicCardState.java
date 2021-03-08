5777376d4452804ab9a279c0966717954b8bd819
Alberto <albicento.ac@gmail.com>
2021-03-08 08:21:48 +0100

    Working on PublicCardState and CardState

--- src/ch/epfl/tchu/game/PublicCardState.java
+++ src/ch/epfl/tchu/game/PublicCardState.java
@@ -1,33 +1,48 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PublicCardState {
-    public PublicCardState(List<Card> faceUpCards, int deckSize, int discardsSize){
+    private final int discardsSize;
+    private final int deckSize;
+    private final List<Card> faceUpCards; //TODO check immutability in other classes
 
+    public PublicCardState(List<Card> faceUpCards, int deckSize, int discardsSize){
+        Preconditions.checkArgument(faceUpCards.size() == 5);
+        Preconditions.checkArgument(deckSize >= 0 || discardsSize >= 0);
+        //TODO check nonNull faceUpCards?
+        //TODO what if less elements faceUpCards
+        this.faceUpCards = faceUpCards;
+        this.discardsSize = discardsSize;
+        this.deckSize = deckSize;
     }
 
     public int totalSize(){
-        return 0; //TODO
+        return faceUpCards.size() + this.deckSize + this.discardsSize;
     }
 
     public List<Card> faceUpCards(){
-        return null; //TODO
+        return new ArrayList<>(this.faceUpCards);
     }
 
     public Card faceUpCard(int slot){
-        return null; //TODO
+        int index = Objects.checkIndex(slot, this.faceUpCards.size());
+        return this.faceUpCards.get(index);
     }
 
     public int deckSize(){
-        return 0; //TODO
+        return deckSize;
     }
 
     public boolean isDeckEmpty(){
-        return false; //TODO
+        return deckSize == 0;
     }
 
     public int discardsSize(){
-        return 0; //TODO
+        return discardsSize;
     }
 }
