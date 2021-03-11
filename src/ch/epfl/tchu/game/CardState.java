e249fb8f6ddecfda284040bea03e6f036fb8f0c4
Alberto <albicento.ac@gmail.com>
2021-03-11 13:07:22 +0100

    CardState documented

--- src/ch/epfl/tchu/game/CardState.java
+++ src/ch/epfl/tchu/game/CardState.java
@@ -19,6 +19,12 @@ public final class CardState extends PublicCardState {
         this.discards = discards;
     }
 
+    /**
+     * Static CardState "builder" that returns a CardState instance with
+     * the first five cards as the face-up cards and an empty discards SortedBag
+     * @param deck a Deck instance with more than 5 cards
+     * @return the instance of CardState built as in the description
+     */
     public static CardState of(Deck<Card> deck){
         Preconditions.checkArgument(deck.size() >= 5);
         List<Card> topCards = deck.topCards(5).toList();
@@ -27,6 +33,12 @@ public final class CardState extends PublicCardState {
         return new CardState(topCards, actualDeck, discards);
     }
 
+    /**
+     * Creates a new CardState instance with a card in the
+     * face-up cards list replaced with one from the deck
+     * @param slot the index (between 0 and 5) of the card to replace
+     * @return the instance of CardState built as in the description
+     */
     public CardState withDrawnFaceUpCard(int slot){
         Preconditions.checkArgument(this.deck.size() > 0);
         int index = Objects.checkIndex(slot, this.faceUpCards().size());
@@ -37,16 +49,30 @@ public final class CardState extends PublicCardState {
         return new CardState(topCards, withoutTopCard, this.discards);
     }
 
+    /**
+     * Getter for the card on the top of the deck (if not empty)
+     * @return the Card instance of the first card in the deck
+     */
     public Card topDeckCard(){
         Preconditions.checkArgument(this.deck.size() > 0);
         return this.deck.topCard();
     }
 
+    /**
+     * Creates a new CardState instance with the first card in the
+     * deck removed
+     * @return the instance of CardState built as in the description
+     */
     public CardState withoutTopDeckCard(){
         Preconditions.checkArgument(this.deck.size() > 0);
         return new CardState(this.faceUpCards(), this.deck.withoutTopCard(), this.discards);
     }
 
+    /**
+     * Recreates the deck starting from the card in discards
+     * @param rng the random generator used to shuffle the deck
+     * @return the instance of CardState built as in the description
+     */
     public CardState withDeckRecreatedFromDiscards(Random rng) {
         Preconditions.checkArgument(this.deck.size() == 0);
         List<Card> listFromDiscards = this.discards.toList();
@@ -54,6 +80,12 @@ public final class CardState extends PublicCardState {
         return new CardState(this.faceUpCards(), newShuffledDeck, SortedBag.<Card>of());
     }
 
+    /**
+     * Creates a new CardState instance with additionalDiscards added
+     * to the discards SortedBag
+     * @param additionalDiscards the card to add to discards
+     * @return the instance of CardState built as in the description
+     */
     public CardState withMoreDiscardedCards(SortedBag<Card> additionalDiscards){
         return new CardState(this.faceUpCards(), this.deck, this.discards.union(additionalDiscards));
     }
