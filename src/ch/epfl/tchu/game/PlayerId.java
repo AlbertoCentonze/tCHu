dd6bc8601d6fbbc463700459c038b66764fa3e40
Alberto <albicento.ac@gmail.com>
2021-03-15 15:56:19 +0100

    Create PlayerId.java

---
+++ src/ch/epfl/tchu/game/PlayerId.java
@@ -0,0 +1,20 @@
+package ch.epfl.tchu.game;
+
+import java.util.List;
+
+public enum PlayerId {
+    PLAYER_1, PLAYER_2;
+
+    // A list containing all the players
+    public static final List<PlayerId> ALL = List.of(PlayerId.values());
+    // The number of players in game
+    public static final int COUNT = ALL.size();
+
+    /**
+     * Get the next turn's player
+     * @return the corresponding PlayerId instance
+     */
+    public PlayerId next(){
+        return this == PLAYER_1 ? PLAYER_2 : PLAYER_1;
+    }
+}
