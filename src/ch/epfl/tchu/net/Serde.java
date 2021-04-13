845f7b206685ca68f844d5e275d345f572a39ff3
Alberto <albicento.ac@gmail.com>
2021-04-13 11:08:28 +0200

    Completed Serde interface and adding new Serdes

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -1,5 +1,12 @@
 package ch.epfl.tchu.net;
 
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public interface Serde<T> {
     /**
      * Methods that serialize an element of type T
@@ -14,4 +21,57 @@ public interface Serde<T> {
      * @return the corresponding element that was originally serialized
      */
     abstract public T deserialize(String toDeserialize);
+
+    public static <T> Serde<T> of(Function<T, String> serializer, Function<String, T> deserializer){
+        return new Serde<T>() {
+            @Override
+            public String serialize(T toSerialize) {
+                return serializer.apply(toSerialize);
+            }
+
+            @Override
+            public T deserialize(String toDeserialize) {
+                return deserializer.apply(toDeserialize);
+            }
+        };
+    }
+    public static <T> Serde<T> oneOf(List<T> enumList){
+        return new Serde<T>() {
+            @Override
+            public String serialize(T toSerialize) {
+                return Integer.toString(enumList.indexOf(toSerialize));
+            }
+
+            @Override
+            public T deserialize(String toDeserialize) {
+                return enumList.get(Integer.parseInt(toDeserialize));
+            }
+        };
+    }
+    public static <T> Serde<List<T>> listOf(Serde<T> serde, String separator){
+        return new Serde<List<T>>() {
+            @Override
+            public String serialize(List<T> toSerialize) {
+                return toSerialize.stream().map(serde::serialize).collect(Collectors.joining(separator));
+            }
+
+            @Override
+            public List<T> deserialize(String toDeserialize) {
+                return Arrays.stream(toDeserialize.split(separator)).map(serde::deserialize).collect(Collectors.toList());
+            }
+        };
+    }
+    public static <T extends Comparable<T>> Serde<SortedBag<T>> bagOf(Serde<T> serde, String separator){
+        return new Serde<SortedBag<T>>() {
+            @Override
+            public String serialize(SortedBag<T> toSerialize) {
+                return toSerialize.stream().map(serde::serialize).collect(Collectors.joining(separator));
+            }
+
+            @Override
+            public SortedBag<T> deserialize(String toDeserialize) {
+                return SortedBag.of(Arrays.stream(toDeserialize.split(separator)).map(serde::deserialize).collect(Collectors.toList()));
+            }
+        };
+    }
 }
---
+++ src/ch/epfl/tchu/net/Serdes.java
@@ -0,0 +1,25 @@
+package ch.epfl.tchu.net;
+
+import ch.epfl.tchu.game.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+
+public class Serdes {
+    private Serdes() {} //TODO
+
+    public static final Serde<Integer> INTEGER_SERDE = Serde.of(n -> Integer.toString(n), Integer::parseInt);
+    public static final Serde<String> STRING_SERDE = Serde.of(
+                    // encoding function
+                    string -> Base64.getEncoder().encodeToString(string.getBytes(StandardCharsets.UTF_8)),
+                    // decoding function
+                    encodedString -> Arrays.toString(Base64.getDecoder().decode(encodedString)));
+    public static final Serde<PlayerId> PLAYER_ID_SERDE = Serde.oneOf(PlayerId.ALL);
+    public static final Serde<Player.TurnKind> TURN_KIND_SERDE = Serde.oneOf(Player.TurnKind.ALL);
+    public static final Serde<Card> CARD_SERDE = Serde.oneOf(Card.ALL);
+    public static final Serde<Route> ROUTE_SERDE = Serde.oneOf(ChMap.routes());
+    public static final Serde<Ticket> TICKET_SERDE = Serde.oneOf(ChMap.tickets());
+    public static final Serde<List<String>> LIST_OF_STRING_SERDE = Serde.listOf(STRING_SERDE, ",");
+}
