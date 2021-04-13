fe97f950a56ad4e93d6525947f9957c62be1d51c
EmmaPoggiolini <79009806+EmmaPoggiolini@users.noreply.github.com>
2021-04-13 22:40:31 +0200

    Modified comments in Deck and Player

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -16,18 +16,18 @@ public final class Deck <C extends Comparable<C>> {
 
     /**
      * Constructor internal to Deck
-     * @param shuffledDeck
+     * @param shuffledDeck : deck of shuffled cards
      */
     private Deck(List<C> shuffledDeck){
-        deck = List.copyOf(shuffledDeck);
+        this.deck = List.copyOf(shuffledDeck);
     }
 
     /**
      * Create a deck with shuffled cards
-     * @param cards
-     * @param rng
-     * @param <C> type
-     * @return
+     * @param cards : cards in the deck
+     * @param rng : randomizer
+     * @param <C> type : type contained in the deck
+     * @return (Deck<C>) new deck
      */
     public static <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng){
         List<C> deck = cards.toList();
@@ -40,7 +40,7 @@ public final class Deck <C extends Comparable<C>> {
      * @return (int) size of the deck
      */
     public int size(){
-        return deck.size();
+        return this.deck.size();
     }
 
     /**
@@ -48,7 +48,7 @@ public final class Deck <C extends Comparable<C>> {
      * @return (boolean) true if deck is empty
      */
     public boolean isEmpty(){
-        return deck.isEmpty();
+        return this.deck.isEmpty();
     }
 
     /**
@@ -58,7 +58,7 @@ public final class Deck <C extends Comparable<C>> {
     public C topCard(){
         // check that the deck isn't empty
         Preconditions.checkArgument(!isEmpty());
-        return deck.get(0);
+        return this.deck.get(0);
     }
 
     /**
@@ -67,23 +67,24 @@ public final class Deck <C extends Comparable<C>> {
      */
     public Deck<C> withoutTopCard(){
         Preconditions.checkArgument(!isEmpty());
-        return new Deck<>(deck.subList(1,size()));
+        return new Deck<>(this.deck.subList(1,size()));
     }
 
     /**
      * Return the first count cards at the top of the deck
-     * @param count (number of cards to return)
+     * @param count : number of cards to return
      * @return (SortedBag<C>) first count cards
      */
     public SortedBag<C> topCards(int count){
         // check count is between 0 and the size of the deck (included)
         Preconditions.checkArgument(count >= 0 && count <= size());
-        return SortedBag.of(deck.subList(0, count));
+        return SortedBag.of(this.deck.subList(0, count));
     }
 
     /**
      * Create new deck without the top cards
-     * @return (Deck<C>) deck without the top cards
+     * @param count : number of cards to take away
+     * @return (Deck<C>) deck without the top (count) cards
      */
     public Deck<C> withoutTopCards(int count){
         // check count is between 0 and the size of the deck (included)
