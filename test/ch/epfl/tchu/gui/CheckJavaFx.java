7ff5e20dfe5856e1a51a5a7eb7faa4ef292b7a04
Alberto <albicento.ac@gmail.com>
2021-04-27 19:21:36 +0200

    Added resources and set up git with javafx
    Also added a file to check that JavaFx is correctly installed

---
+++ test/ch/epfl/tchu/gui/CheckJavaFx.java
@@ -0,0 +1,10 @@
+package ch.epfl.tchu.gui;
+
+import javafx.scene.paint.Color;
+
+public final class CheckJavaFx {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
