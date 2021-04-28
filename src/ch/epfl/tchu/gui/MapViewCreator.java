e974473f02b9f7ae9fac7ce8bbf443d45e780503
Alberto <albicento.ac@gmail.com>
2021-04-28 19:22:14 +0200

    Now the full map is correctly drawn

--- src/ch/epfl/tchu/gui/MapViewCreator.java
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -2,6 +2,8 @@ package ch.epfl.tchu.gui;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.Route;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
@@ -10,9 +12,37 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MapViewCreator {
     private MapViewCreator() {}
+
+    private static Node createNodeFromRoute(Route r){
+        Group routeNode = new Group();
+        routeNode.setId(r.id());
+        routeNode.getStyleClass()
+                .addAll("route",
+                        r.level().name(),
+                        r.color() == null ? "NEUTRAL" : r.color().name());
+        for (int i = 1; i <= r.length(); ++i){
+            Group case1 = new Group(); //TODO find a better name for case
+            Group wagonGroup = new Group();
+            Circle wagonCircle1 = new Circle(3);
+            Circle wagonCircle2 = new Circle(3);
+            Rectangle wagonRectangle = new Rectangle(36, 12);
+            Rectangle rail = new Rectangle(36, 12);
+            case1.setId(String.format("%s_%s", r.id(), i));
+            rail.getStyleClass()
+                    .addAll("track", "filled");
+            wagonGroup.getStyleClass().add("car");
+            wagonRectangle.getStyleClass().add("filled");
+            wagonGroup.getChildren()
+                    .addAll(wagonCircle1, wagonCircle2, wagonRectangle);
+            case1.getChildren().addAll(wagonGroup, rail);
+            routeNode.getChildren().add(case1);
+        }
+        return routeNode;
+    }
 
     public static Node createMapView(){
         Pane gameMap = new Pane();
@@ -20,27 +50,8 @@ public class MapViewCreator {
                 .addAll("colors.css", "map.css");
         ImageView backgroundImage = new ImageView();
         gameMap.getChildren().add(backgroundImage);
-        Group route = new Group();
-        gameMap.getChildren().add(route);
-        route.setId("AT1_STG_1");
-        route.getStyleClass()
-                .addAll("route", "UNDERGROUND",
-                        "NEUTRAL");
-        Group casewtf = new Group();
-        casewtf.setId("AT1_STG_1_1");
-        route.getChildren().add(casewtf);
-        Rectangle railNotRail = new Rectangle(36, 12);
-        railNotRail.getStyleClass()
-                .addAll("track", "filled");
-        casewtf.getChildren().add(railNotRail);
-        Group wagonGroup = new Group();
-        wagonGroup.getStyleClass().add("car");
-        Circle wagonCircle1 = new Circle(3);
-        Circle wagonCircle2 = new Circle(3);
-        Rectangle wagonRectangle = new Rectangle(36, 12);
-        wagonRectangle.getStyleClass().add("filled");
-        wagonGroup.getChildren()
-                .addAll(wagonCircle1, wagonCircle2, wagonRectangle);
+        List<Node> nodes = ChMap.routes().stream().map(MapViewCreator::createNodeFromRoute).collect(Collectors.toList());
+        gameMap.getChildren().addAll(nodes);
         return gameMap;
     }
 }
