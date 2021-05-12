cdeb41ad149facf26b82ea8d91fa0250a751642b
Alberto <albicento.ac@gmail.com>
2021-05-12 14:58:36 +0200

    Added sigcheck for the final submission

---
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -0,0 +1,13 @@
+package ch.epfl.tchu.gui;
+
+import javafx.stage.Stage;
+
+public class ClientMain {
+    public static void main(String[] args){
+
+    }
+
+    public void start(Stage stage){
+
+    }
+}
