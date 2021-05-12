50689dc74a4eecfbc8dc2ebd905482a1c149a82f
Alberto <albicento.ac@gmail.com>
2021-05-12 12:01:01 +0200

    Fixed failing tests and removed unnecessary ones

--- test/ch/epfl/tchu/gui/InfoTest.java
+++ test/ch/epfl/tchu/gui/InfoTest.java
@@ -342,7 +342,7 @@ public class InfoTest {
         var info = new Info("Martin");
 
         var actual1 = info.lastTurnBegins(0);
-        var expected1 = "\nMartin n'a plus que 0 wagons, le dernier tour commence !\n";
+        var expected1 = "\nMartin n'a plus que 0 wagon, le dernier tour commence !\n";
         assertEquals(expected1, actual1);
 
         var actual2 = info.lastTurnBegins(1);
