0595a8225560aa49b5eaad43c1e35687917c49db
Alberto <albicento.ac@gmail.com>
2021-05-12 14:58:36 +0200

    Added sigcheck for the final submission

---
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -0,0 +1,11 @@
+package ch.epfl.tchu.gui;
+
+import javafx.stage.Stage;
+
+public class ServerMain {
+    public static void main(String[] args){}
+
+    public void start(Stage stage){
+
+    }
+}
