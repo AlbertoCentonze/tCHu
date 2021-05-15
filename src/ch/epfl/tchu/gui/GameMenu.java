4d76feb6b6250e8a64c9b3aec54be84d2c380af8
Alberto <albicento.ac@gmail.com>
2021-05-15 08:40:27 +0200

    Create GameMenu.java

---
+++ src/ch/epfl/tchu/gui/GameMenu.java
@@ -0,0 +1,13 @@
+package ch.epfl.tchu.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameMenu extends Application {
+    public static void main(String[] args){ launch(args); }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
